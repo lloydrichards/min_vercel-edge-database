c2a41ea71c2637dec5f76bc50a981ac99acdd78e
lloydrichards <lloyd.d.richards@gmail.com>
2025-04-27 21:56:38 +0200

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,10 +6,7 @@ const globalForDb = globalThis as unknown as { client: Client | undefined };
 
 const initClient = () => {
   const dbPath = path.join(process.cwd(), "database", "db.sqlite");
-  const exists = readdirSync(path.join(process.cwd(), "database"));
-  if (!exists) {
-    throw new Error(`Database file "db.sqlite" does not exist`);
-  }
+
   return globalForDb.client ?? createClient({ url: `file:${dbPath}` });
 };
 
