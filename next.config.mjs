d6dda292e6223b8312c9ec573149704215880e89
lloydrichards <lloyd.d.richards@gmail.com>
2025-04-27 21:51:25 +0200

    feat: add outputFileTracingIncludes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingIncludes: {
+    database: ["./database/**/*"],
+    "database-broke": ["./database-broke/**/*"],
+  },
+};
 
 export default nextConfig;
